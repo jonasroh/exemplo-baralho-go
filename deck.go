a60ae90611e0250648a3a72e9450d7e3f187b4d5
JONAS RODRIGUES <jonas.s.rodrigues@outlook.com>
2023-03-07 20:50:52 -0300

    salvando deck

--- deck.go
+++ deck.go
@@ -1,14 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"strings"
+)
 
 // Criando um novo tipo de 'deck'
 // que se trata de um slice de strings
 type deck []string
 
+func novoDeck() deck {
+	cartas := deck{}
+
+	naipesCarta := []string{"Espadas", "Paus", "Copas", "Ouros"}
+	valoresCarta := []string{"Às", "Dois", "Três", "Quatro", "Cinco", "Seis", "Sete", "Oito", "Nove", "Dez", "Valete", "Dama", "Rei"}
+
+	for _, naipe := range naipesCarta {
+		for _, valor := range valoresCarta {
+			cartas = append(cartas, valor+" de "+naipe)
+		}
+	}
+	cartas = append(cartas, "Coringa")
+	return cartas
+}
+
 func (d deck) print() {
 	for i, carta := range d {
 		fmt.Println(i, carta)
 	}
+}
 
+func embaralhar(d deck, tamanhoMao int) (deck, deck) {
+	return d[:tamanhoMao], d[tamanhoMao:]
+}
+
+func (d deck) transformaEmString() string {
+	return strings.Join([]string(d), ",")
+}
+
+// func para salvar o baralho em um arquivo
+func (d deck) salvaEmArquivo(nomeArquivo string) error {
+	return ioutil.WriteFile(nomeArquivo, []byte(d.transformaEmString()), 0666)
 }
--- main.go
+++ main.go
@@ -1,15 +1,6 @@
 package main
 
 func main() {
-	// criando um slice
-	cartas := deck{"Rei de Espadas", novaCarta()}
-
-	//adicionando um novo elemento
-	cartas = append(cartas, "Três de Ouros")
-
-	cartas.print()
-}
-
-func novaCarta() string {
-	return "Cinco de Copas"
+	cartas := novoDeck()
+	cartas.salvaEmArquivo("minhas_cartas")
 }
